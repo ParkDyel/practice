a6f7ab7f7496135950f685aa062d352c26316966
ParkDyel <bluetibe@gmail.com>
2017-02-06 20:34:44 +0900

    it's hard to understand component

---
+++ JS/electron/displayElectron/src/components/loginModal.vue
@@ -0,0 +1,36 @@
+Vue.component('fake-nav', { 
+  template:'<div id="fake-nav"><a href="#register" @click="open(1, $event)">Register</a><a href="#login" @click="open(2, $event)">Login</a></div>',
+})
+
+var nav = new Vue({
+  el: '#header',
+  data :function(){
+    return {
+      name : 0
+    }
+  },
+  methods: {
+    open: function (which, e) {
+      console.log(e);
+      switch(which){
+        case 1:
+          which='regiter'
+          break;
+        case 2:
+          which='login'
+          break;
+        defalut:
+          break;
+      }
+      e.preventDefault();
+      if (modal.active !== null) {
+        $('#form-' + modal.active).removeClass('active');
+        $('#' + modal.active + '-form').removeClass('active');
+      }
+      $('#login-modal').addClass('active');
+      $('#form-' + which).addClass('active');
+      $('#' + which + '-form').addClass('active');
+      modal.active = which;
+    },
+  }
+});
--- docs/list.md
+++ docs/list.md
@@ -1,2 +1,10 @@
+
+[자이언트 킬링:344, 2017.02.06](http://www.marumaru.in/b/manga/187329)
+
 [마탄의 왕과 바나디스](http://marumaru.in/?c=1/30&cat=%EC%9B%94%EA%B0%84&uid=82453)
+[츠구모모:65, 2017.02.06](http://marumaru.in/b/manga/124411)
+[유라기장의 유우나:49, 2017.02.04](http://www.marumaru.in/b/manga/129371)
+
 [변녀:41](http://marumaru.in/?c=1/31&cat=%EA%B2%A9%EC%9B%94%2F%EB%B9%84%EC%A0%95%EA%B8%B0&mod=view&uid=56830)
+[아들의 아내:39, 2017.02.06](http://marumaru.in/?c=1/30&cat=%EC%9B%94%EA%B0%84&mod=view&uid=59652)
+[내가 아가씨 학교에 서민 샘플로 납치당한 사건:61, 2017.02.04](http://marumaru.in/b/manga/61487)
