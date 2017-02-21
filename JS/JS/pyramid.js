3f73fac80616a91872f0f2ce4b5428549982604a
Park DongYoung <bluetibe@gmail.com>
2017-02-21 22:17:13 +0900

    learning about JS5

---
+++ JS/JS/pyramid.js
@@ -0,0 +1,50 @@
+var strbuf;
+var height
+var width=0
+
+height = 16
+
+// for (var i = 0; i < height; i++) {
+//   strbuf = ''
+//   for (var k = 0; k < height - i; k++) {
+//     strbuf += ' '
+//   }
+//   for (var k = 0; k < width; k++) {
+//     strbuf += '*'
+//   }
+//   for (var k = 0; k < height - i; k++) {
+//     strbuf += ' '
+//   }
+//   console.log(strbuf);
+//   width++;
+// }
+
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+
+// for (var i = 0; i < height; i++) {
+//   strbuf = ''
+//   for (var k = 0; k < height - i; k++) {
+//     strbuf += ' '
+//   }
+//   for (var k = 0; k < width-1; k++) {
+//     strbuf += '*'
+//   }
+//   if(i!=0) strbuf = strbuf + '*' + reverse(strbuf)
+//   console.log(strbuf);
+//   width++;
+// }
+
+for (var i = 0; i < height; i++) {
+  strbuf = ''
+  for (var k = 0; k < height - i; k++) {
+    strbuf += ' '
+  }
+  for (var k = 0; k < i-1; k++) {
+    strbuf += '*'
+  }
+  if(i!=0) strbuf = strbuf + '*' + reverse(strbuf)
+  console.log(strbuf);
+  width++;
+}
