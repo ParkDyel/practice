159862c8f94d3d0bad02d32010b520ed8d5b1bfd
Unknown <bluetibe@gmail.com>
2017-03-20 23:10:29 +0900

    Code:99

---
+++ JS/JS/02_basic.js
@@ -0,0 +1,15 @@
+// // equal window.onload(), third is shortCut
+// $(document).ready(function () {
+//   console.log('first print')
+// })
+// $(document).ready(function () {
+//   console.log('second print')
+// })
+// $(function () {
+//   console.log('third print')
+// })
+
+// // wildcard selector 
+// $('*').css('color', 'cyan');
+
+
