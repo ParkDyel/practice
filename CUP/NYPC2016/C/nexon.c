293363771f1a3208192a02267639366b001ac2d8
ParkDyel <bluetibe@gmail.com>
2017-09-04 18:51:34 +0900

    Code 22
    About NYPC 2016 fields

--- CUP/NYPC2016/C/nexon.c
+++ CUP/NYPC2016/C/nexon.c
@@ -6,27 +6,25 @@
 
 int getArraySize(void);
 int setArray(int size);
-int freeTwoDimensionalArray(int address, int size);
+int freeTwoDimensionalArray(char** address, int size);
 
 int main(int argc, char argv[]){
 
   printf("Program Working");
 
-  printf("Get Array Size:Start\n");
   int arraySize = getArraySize();
   if(arraySize>20){
     printf("Array Size is bigger than limits\n");
     return 0;
   }
-  printf("Get Array Size:Done\n");
   printf("Array Size is %d", arraySize);
 
-  printf("Set Array:Start\n");
-  int ptr_TwoDimensionalArray = setArray(arraySize);
+  char** ptr_TwoDimensionalArray = NULL;
+  ptr_TwoDimensionalArray = setArray(arraySize);
   printf("Set Array:Done.\n Address : %p\n", ptr_TwoDimensionalArray);
 
-  printf("Free Array:Start\n");
   freeTwoDimensionalArray(ptr_TwoDimensionalArray, arraySize);
+  
   printf("Free Array:Done\n");
 
   printf("Program exit succesfully");
@@ -34,14 +32,22 @@ int main(int argc, char argv[]){
 }
 
 int getArraySize(void){
+
+  printf("Get Array Size:Start\n");
+
   int temp_sizeOfArray;
   printf("Plz Input Array size\n");
   scanf("%d", &temp_sizeOfArray);
+
+  printf("Get Array Size:Done\n");
 
   return temp_sizeOfArray;
 }
 
 int setArray(int size){
+
+  printf("Set Array:Start\n");
+
   int idx;
   char **arr;
   arr = (char **) malloc (sizeof(char*)*size);
@@ -49,15 +55,20 @@ int setArray(int size){
     arr[idx]= (char*) malloc (sizeof(char)*size);
   }
 
-  return arr
+  return (char**) arr;
 } 
 
-int freeTwoDimensionalArray(int address, int size){
+int freeTwoDimensionalArray(char** address, int size){
+
+  printf("Free Array:Start\n");
+
   int idx;
   for(idx=0;idx<size;idx++){
     free(address[idx]);
   }
   free(address);
+
+  return 0;
 }
 /*
   // get stdin as fgets
