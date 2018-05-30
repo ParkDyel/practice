c270a1ed518d1f65fd13d991ed6e69d5a7050e1d
ParkDyel <bluetibe@gmail.com>
2018-05-30 20:18:42 +0900

    Code 99
    Need Merge!

---
+++ JS/front-end/@Library/vuejs/map/src/pages/Map.vue
@@ -0,0 +1,16 @@
+<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=095dc1fc05ddb0763c808feff85c276b"></script>
+<template>
+  <main-layout>
+    <div id="map" style="width:500px; height:400px;"></div>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
