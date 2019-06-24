57a9da55a39fd23faa63552a6f455153737d3c0b
ParkDyel <bowbow180508@gdnet.co.kr>
2019-06-25 05:38:23 +0900

    zabbix server

--- Infra/serverMonitoring/zabbix/build.bash
+++ Infra/serverMonitoring/zabbix/build.bash
@@ -3,10 +3,19 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo reboot
 docker pull zabbix/zabbix-appliance
-docker run --name some-zabbix-appliance -p 80:80 -p 10051:10051 -d zabbix/zabbix-appliance
+# docker run --name zabbix-appliance --restart always -p 80:80 -p 10051:10051 -d zabbix/zabbix-appliance 
+docker run --name zabbix-appliance --restart always -p 80:80 -p 10051:10051 -d zabbix/zabbix-appliance
+
+sudo vim /etc/postfix/main.cf
+```bash
+mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 172.16.0.0/12
+inet_interfaces = all
+```
+sudo service postfix restart
+172.16.0.1
 
 docker pull zabbix/zabbix-agent
-docker run --name some-zabbix-agent --link some-zabbix-appliance:zabbix-server -d zabbix/zabbix-agent
+docker run --name zabbix-agent -p 10050:10050 -e ZBX_ACTIVE_ALLOW=true --link zabbix-appliance:zabbix-server --restart always -d zabbix/zabbix-agent
 
 # zabbix server admin tool에서 ip 설정
 docker exec some-zabbix-agent 
